a9a332623f2ea594a728021824097f09c2076898
Pedro Carnio | Shadow_Voidh <shadow.voidh@gmail.com>
2026-07-22 02:25:31 +0000

    Sadistic Shadow

--- src/boot.asm
+++ src/boot.asm
@@ -1,25 +1,11 @@
 bits 32
-section .multiboot
-    align 4
-    dd 0x1BADB002              ; Magic Number do padrão Multiboot
-    dd 0x00                    ; Flags
-    dd - (0x1BADB002 + 0x00)   ; Checksum de validação
 
-section .text
-global _start
-extern kernel_main
-
-_start:
-    cli                        ; Desabilita interrupções de hardware
-    call kernel_main           ; Chama o ponto de entrada 
-    hlt                        ; Trava a CPU caso a função C++ retorne
-
-
-    MBALIGN     equ  1 << 0                   ; Alinha módulos carregados em páginas
-MEMINFO     equ  1 << 1                   ; Fornece mapa de memória
-VIDMOD      equ  1 << 2                   ; SOLICITA MODO GRÁFICO AO BOOTLOADER
-FLAGS       equ  MBALIGN | MEMINFO | VIDMOD
-MAGIC       equ  0x1BADB002
+; --- Configuração do Cabeçalho Multiboot ---
+MBALIGN     equ 1 << 0                   ; Alinha módulos carregados em páginas
+MEMINFO     equ 1 << 1                   ; Fornece mapa de memória
+VIDMOD      equ 1 << 2                   ; Solicita modo gráfico ao bootloader
+FLAGS       equ MBALIGN | MEMINFO | VIDMOD
+MAGIC       equ 0x1BADB002
 CHECKSUM    equ -(MAGIC + FLAGS)
 
 section .multiboot
@@ -28,36 +14,40 @@ align 4
     dd FLAGS
     dd CHECKSUM
     
-    ; Campos de endereço (não usados quando compilado em ELF)
+    ; Campos de endereço (não usados quando compilado em formato ELF)
     dd 0, 0, 0, 0, 0
     
     ; Configurações do Modo de Vídeo VBE
-    dd 0                          ; 0 = Modo Gráfico (Linear Framebuffer)
-    dd 1024                       ; Largura (Width)
-    dd 768                        ; Altura (Height)
-    dd 32                         ; BPP (Bits Per Pixel)
+    dd 0                         ; 0 = Modo Gráfico (Linear Framebuffer)
+    dd 1024                      ; Largura (Width)
+    dd 768                       ; Altura (Height)
+    dd 32                        ; BPP (Bits Per Pixel)
 
+; --- Pilha de Memória (Stack) ---
 section .bss
 align 16
 stack_bottom:
-    resb 16384 ; 16 KB de pilha
+    resb 16384                   ; Reservar 16 KB de pilha
 stack_top:
 
+; --- Código Executável ---
 section .text
-global _start:function (_start.end - _start)
+global _start
 extern kernel_main
 
 _start:
+    ; Configura a pilha de execução
     mov esp, stack_top
 
-    ; O GRUB passa um ponteiro para a estrutura Multiboot no registrador EBX
+    ; O GRUB passa o ponteiro da estrutura Multiboot em EBX.
+    ; Colocamos no registrador para ser lido como argumento no C++:
     push ebx
+
+    ; Chama o kernel em C++
     call kernel_main
 
+    ; Caso o kernel_main retorne, desabilita interrupções e trava a CPU
     cli
 .hang:
     hlt
     jmp .hang
-.end:
-
-section .note.GNU-stack noalloc noexec nowrite progbits
